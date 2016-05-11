a7918f876f99ae60590ed1f6c7518c5177cc00c7
syamsulHuda <syamsulHuda>
2016-05-11 15:37:32 +0700

    Kerjaan Tadi siang :(

--- Login/src/sample/Controller.java
+++ Login/src/sample/Controller.java
@@ -1,5 +1,68 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
-    
+    @FXML private TextField user;
+    @FXML private TextField password;
+    @FXML private Button loginButton;
+
+    public void initialize() {}
+
+    public void initManager(final LoginManager loginManager) {
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent event) {
+                String sessionID = authorize();
+                if (sessionID != null) {
+                    loginManager.authenticated(sessionID);
+                }
+            }
+        });
+    }
+
+    /**
+     * Check authorization credentials.
+     *
+     * If accepted, return a sessionID for the authorized session
+     * otherwise, return null.
+     */
+    private String authorize() {
+        return
+                "open".equals(user.getText()) && "sesame".equals(password.getText())
+                        ? generateSessionID()
+                        : null;
+    }
+
+    private static int sessionID = 0;
+
+    private String generateSessionID() {
+        sessionID++;
+        return "xyzzy - session " + sessionID;
+    }
+
+    //for Main Menu Controller
+    @FXML private Button logoutButton;
+    //@FXML private Label  sessionLabel;
+
+    //public void initialize() {}
+
+    public void initSessionID(final LoginManager loginManager, String sessionID) {
+        //sessionLabel.setText(sessionID);
+        logoutButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent event) {
+                loginManager.logout();
+            }
+        });
+    }
 }
--- Login/src/sample/Main.java
+++ Login/src/sample/Main.java
@@ -10,24 +10,35 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("menuUtama.fxml"));
+    /*@Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Merpati Putih");
 
         primaryStage.setScene(new Scene(root, 600, 400));
+
         primaryStage.show();
+    }*/
+    public static void main(String[] args) { launch(args); }
+    @Override public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(new StackPane());
+
+        LoginManager loginManager = new LoginManager(scene);
+        loginManager.showLoginScreen();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
